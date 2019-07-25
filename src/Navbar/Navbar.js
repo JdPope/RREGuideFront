ef7f0f3e5f0be7cad850e2f7e1fe80f6f8ed4edb
Jessica Pope <jessica.pope.co@gmail.com>
2019-07-25 17:15:20 -0600

    stying changes

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -8,14 +8,16 @@ const Navbar = props =>{
 return(
   <header className="Navbar">
     <nav className="Navbar_nav">
+
+      <div className="Navbar_logo"><a href="/">RRE G</a></div>
+      <div className="spacer"/>
       <div>
         <ToggleButton click={props.drawerClickHandler}/>
       </div>
-      <div className="Navbar_logo"><a href="/">RREGuide</a></div>
-      <div className="spacer"/>
       <div className="Navbar_items">
+
         <ul>
-          <li><Link to={'/LoginPage'} className="nav-link">Login</Link></li>
+          <li><Link to={'/LoginPage'} className="nav-link">Sign In</Link></li>
         </ul>
       </div>
     </nav>
