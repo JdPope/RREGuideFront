013fe27d430a2ef9bdc94e26878d67760ab043ce
Jessica Pope <jessica.pope.co@gmail.com>
2019-07-29 20:44:40 -0600

    Refactored RREItem into RREJob

--- src/RREContainer/RREContainer.js
+++ src/RREContainer/RREContainer.js
@@ -1,13 +1,13 @@
 import React, {Component} from 'react'
 import { BrowserRouter as Router,Link } from 'react-router-dom';
 import './RREContainer.css'
-import RREItem from '../RREItem/RREItem'
+import RREJob from '../RREJob/RREJob'
 
 export default function RREContainer(props) {
-console.log("keys.length", Object.keys(props.jobs).length)
+
 
   const displayJobItems = props.jobs.map((job) => {
-    return <RREItem key={job.id} job={job}/>
+    return <RREJob key={job.id} job={job}/>
   })
 
 
--- src/RRERole/RRERole.js
+++ src/RRERole/RRERole.js
@@ -5,7 +5,8 @@ export default function RRERole (props){
 
   return (
     <React.Fragment>
-      <h1>{props.role.role_name}</h1>
+      <h2>Role</h2>
+      <h3>{props.role.role_name}</h3>
 
     </React.Fragment>
   )
