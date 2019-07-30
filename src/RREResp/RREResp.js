b961ad11580419dd6dda76a7f64f7bc2209dfe26
Jessica Pope <jessica.pope.co@gmail.com>
2019-07-29 21:03:21 -0600

    Responsibilities working

---
+++ src/RREResp/RREResp.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function RREResp (props){
+
+
+  return (
+    <React.Fragment>
+      <h2>Responsibility</h2>
+      <h3>{props.resp.resp_name}</h3>
+  
+    </React.Fragment>
+  )
+}
--- src/RRERole/RRERole.js
+++ src/RRERole/RRERole.js
@@ -1,13 +1,18 @@
 import React from 'react'
+import RREResp from '../RREResp/RREResp'
 
 export default function RRERole (props){
+  console.log(props.role)
 
+  const displayRespItems = props.role.responsibilities.map((resp) => {
+      return <RREResp key={resp.resp_id} resp={resp}/>
+    })
 
   return (
     <React.Fragment>
       <h2>Role</h2>
       <h3>{props.role.role_name}</h3>
-
+        {displayRespItems}
     </React.Fragment>
   )
 }
