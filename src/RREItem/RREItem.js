bdc8a1d366f5ddfe569296baa52869eba35e657d
Jessica Pope <jessica.pope.co@gmail.com>
2019-07-29 20:16:04 -0600

    added Role component

--- src/RREItem/RREItem.js
+++ src/RREItem/RREItem.js
@@ -1,13 +1,16 @@
 import React from 'react'
-
+import RRERole from '../RRERole/RRERole'
 export default function RREItem (props){
 
-  
+
+  const displayRoleItems = props.job.rre.role.map((role) => {
+    return <RRERole key={role.id} role={role}/>
+  })
 
   return (
     <React.Fragment>
       <h1>{props.job.name}</h1>
-
+          {displayRoleItems}
     </React.Fragment>
   )
 }
---
+++ src/RRERole/RRERole.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function RRERole (props){
+
+
+  return (
+    <React.Fragment>
+      <h1>{props.role.name}</h1>
+      
+    </React.Fragment>
+  )
+}
