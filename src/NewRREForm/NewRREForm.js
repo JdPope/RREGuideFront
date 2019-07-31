064f3060764cee6d511d502d35b10b9b8d8a267c
Jessica Pope <jessica.pope.co@gmail.com>
2019-07-30 21:44:36 -0600

    add role logic in form

--- src/NewRREForm/NewRREForm.js
+++ src/NewRREForm/NewRREForm.js
@@ -9,8 +9,23 @@ class NewRREForm extends Component {
     this.state = {
       job_id:1,
       job_name:"",
-        roles:[]
+        roles:[{role_id:0}],
+        responsibilities:[{resp_id:0, role_id:0}]
     }
+  }
+
+  addRole = (event) => {
+    this.setState({roles:[...this.state.roles, {role_id: this.state.roles.length}]})
+
+  }
+
+
+
+  listRoles = () => {
+    this.state.roles.map((role) => {
+
+      return <li>some txt to test{role.role_id}</li>
+    })
   }
 
   handleChange = (event) => {
@@ -20,7 +35,32 @@ class NewRREForm extends Component {
       })
   }
 
+  handleRoleChange = (event) =>{
+    const { name, value } = event.target
+    let index = name.substring(5)
+    index = parseInt(index)
+    let roles = this.state.roles
+    roles[index] = {
+      role_id:index,
+      role_name: value}
+    this.setState({
+      roles:roles
+    })
+  }
+
+
   render(props){
+    const rolesList =
+      this.state.roles.map((role) => {
+        const respList = makeRespList(role)
+        return <li>
+        <input name={`role_${role.role_id}`} onChange={this.handleRoleChange} defaultValue={role.role_name}  placeholder={role.role_id}></input>
+        <ul>
+          {respList}
+        </ul>
+        </li>
+      })
+
     const { job_name, expectation_name, resp_name, role_name, role_percentage} = this.state
     return(
       <React.Fragment>
@@ -28,15 +68,11 @@ class NewRREForm extends Component {
               <div className="blerb">Make Role Definition Easy with RRE!</div>
               <form >
                 <label className="SignUpContainer-label">Job Title:</label>
-                <input className="SignUpContainer-input" onChange={this.handleChange} name="job_name" required placeholder= "Job Title" type="text" defaultValue={job_name}/>
-                <label className="SignUpContainer-label">Role Name:</label>
-                <input className="SignUpContainer-input" onChange={this.handleChange} name="role" required placeholder= "Role Name" type="text" value={role_name}/>
-                <label className="SignUpContainer-label">Percentage of Role:</label>
-                <input className="SignUpContainer-input" onChange={this.handleChange} name="role_percentage" required placeholder= "Role Percentage" type="text" value={role_percentage} />
-                <label className="SignUpContainer-label">Responsibilities:</label>
-                <input className="SignUpContainer-input" onChange={this.handleChange} name="Responsibilities" required placeholder= "Responsibility" type="text" value={resp_name}  />
-                <label className="SignUpContainer-label">Expectations:</label>
-                <input className="SignUpContainer-input" onChange={this.handleChange} name="expectations_name" required placeholder= "Expectation" type="text" value={expectation_name}  />
+                <input className="SignUpContainer-input" onChange={this.handleChange} name="job_name"  placeholder= "Job Title" type="text" defaultValue={job_name}/>
+                <ul>
+                  {rolesList}
+                </ul>
+                <button onClick={(e) => {e.preventDefault();this.addRole(e)}}>+ Role</button>
 
               </form>
               <button className="signupbutton" onClick={(e)=> console.log("chris state", this.state)} >Submit form!</button>
