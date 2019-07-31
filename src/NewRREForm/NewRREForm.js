7fdab91e1647e8dd9eea38c73c9b5741f472c079
Jessica Pope <jessica.pope.co@gmail.com>
2019-07-30 21:00:01 -0600

    job_name working

--- src/NewRREForm/NewRREForm.js
+++ src/NewRREForm/NewRREForm.js
@@ -7,17 +7,9 @@ class NewRREForm extends Component {
   constructor(props) {
     super(props)
     this.state = {
-        rre:{
-          role_id:1,
-          role_percentage:"",
-          role_name:"",
-          responsibilities:[
-            {resp_id:1, resp_name:"", expectations:[
-              { expectation_id:1, expectation_name:""}
-              ]
-            }
-          ]
-        }
+      job_id:1,
+      job_name:"",
+        roles:[]
     }
   }
 
@@ -29,14 +21,14 @@ class NewRREForm extends Component {
   }
 
   render(props){
-    const { expectation_name, resp_name, role_name, role_percentage} = this.state
+    const { job_name, expectation_name, resp_name, role_name, role_percentage} = this.state
     return(
       <React.Fragment>
             <div className="SignUpContainer-main">
               <div className="blerb">Make Role Definition Easy with RRE!</div>
               <form >
                 <label className="SignUpContainer-label">Job Title:</label>
-                <input className="SignUpContainer-input" onChange={this.handleChange} name="job" required placeholder= "Job Title" type="text"/>
+                <input className="SignUpContainer-input" onChange={this.handleChange} name="job_name" required placeholder= "Job Title" type="text" defaultValue={job_name}/>
                 <label className="SignUpContainer-label">Role Name:</label>
                 <input className="SignUpContainer-input" onChange={this.handleChange} name="role" required placeholder= "Role Name" type="text" value={role_name}/>
                 <label className="SignUpContainer-label">Percentage of Role:</label>
@@ -45,8 +37,9 @@ class NewRREForm extends Component {
                 <input className="SignUpContainer-input" onChange={this.handleChange} name="Responsibilities" required placeholder= "Responsibility" type="text" value={resp_name}  />
                 <label className="SignUpContainer-label">Expectations:</label>
                 <input className="SignUpContainer-input" onChange={this.handleChange} name="expectations_name" required placeholder= "Expectation" type="text" value={expectation_name}  />
+
               </form>
-              <button className="signupbutton" >Sign Up!</button>
+              <button className="signupbutton" onClick={(e)=> console.log("chris state", this.state)} >Submit form!</button>
             </div>
       </React.Fragment>
     )
